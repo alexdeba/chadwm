b098f0ca0a04447cc4f3fc69942bebced8bb7c34
Alex Deba <alex.globe.trotter@gmail.com>
2026-03-12 22:38:31 +0100

    changes

--- chadwm/scratchpad.c
+++ chadwm/scratchpad.c
@@ -7,17 +7,9 @@ togglescratch(const Arg *arg)
     Arg sparg = {.v = scratchpads[arg->ui].cmd};
 
     for (c = selmon->clients; c && !(found = c->tags & scratchtag); c = c->next);
-    
     if (found) {
         unsigned int newtagset = selmon->tagset[selmon->seltags] ^ scratchtag;
-        
-        // --- LA LIGNE CORRIGÉE EST ICI ---
-        // On nettoie la fenêtre pour qu'elle n'ait QUE le tag du scratchpad.
-        // Cela empêche la fenêtre de rester accrochée au tag du bureau (ex: tag 1)
-        // lorsque l'on masque le scratchpad.
         c->tags = scratchtag;
-        // ---------------------------------
-
         if (newtagset) {
             selmon->tagset[selmon->seltags] = newtagset;
             focus(NULL);
